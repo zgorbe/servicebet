f5786444b66ad56e1d79df4e6424c4a369820c06
Zoli <zoli@ubuntu.ubuntu-domain>
2011-02-11 19:22:12 +0100

    minor changes

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ gem install dm-core --version 0.10.1 --no-ri --no-rdoc
 gem install data_objects --version 0.10.1 --no-ri --no-rdoc
 gem install do_mysql --version 0.10.1 --no-ri --no-rdoc
 gem install do_postgres --version 0.10.1 --no-ri --no-rdoc
+gem install big_band --version 0.5.0 --no-ri --no-rdoc
