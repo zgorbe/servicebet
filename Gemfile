dc14cecee337a29584fcbd8b191f7d14aa398691
unknown <epamrazor@.(none)>
2011-02-11 07:59:31 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source "http://rubygems.org"
+source "http://gems.github.com"
+
+gem "sinatra ", :git => "git://github.com/sinatra/sinatra.git"
+gem "data_objects", "0.10.1"
+gem "dm-core", "0.10.1"
+
+
+group :development do
+  gem "big_band", "0.5.0"
+  gem "do_mysql", "0.10.1"
+end
+
+
---
+++ servicebet.rb
@@ -0,0 +1,33 @@
+require 'dm-core'
+require 'do_mysql' if development?
+require 'do_postgres' if production?
+require 'partials'
+require 'sinatra/reloader' if development?
+
+
+get "/" do
+  "Welcome to service bet!"
+end
+
+
+configure :development, :test do
+  DataMapper.setup(:default, 'mysql://root@localhost:3306/servicebet?encoding=UTF-8')
+  DataObjects::Mysql.logger = DataObjects::Logger.new(STDOUT,0)
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'] + '?encoding=UTF-8')
+end
+
+
+
+helpers do
+  include Sinatra::Partials
+
+  alias_method :h, :escape_html
+
+  def format_date(date)
+    time = date.utc + (60 * 60 * 2)
+    time.strftime('%Y-%m-%d %H:%M')
+  end
+end
