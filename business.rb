3defa2f0b6ce66d20703fe17103dba1d6aff94f2
unknown <Zoli@.(none)>
2011-02-13 12:28:06 +0100

    Password generate method added

--- business.rb
+++ business.rb
@@ -1,4 +1,10 @@
 module ServiceBet
   module Business
+    def generate_password(length=8)
+      chars = 'abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ23456789'
+      password = ''
+      length.downto(1) { |i| password << chars[rand(chars.length - 1)] }
+      password
+    end
   end
 end
