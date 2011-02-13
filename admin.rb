76bea7c5377f34b06fe0883fddc454815f05bc44
unknown <Zoli@.(none)>
2011-02-13 18:19:14 +0100

    Creating the first user and login

--- admin.rb
+++ admin.rb
@@ -1,3 +1,38 @@
+require 'digest/md5'
+
 get "/admin" do
   erb :admin
+end
+
+get "/admin/newuser" do
+  @user = User.new
+  erb :edituser
+end
+
+post "/admin/users" do
+  @user = User.new(params[:user]) if params[:user]
+  @password = generate_password
+  @user.password = Digest::MD5.hexdigest(@password)
+  @user.last_login_at = Time.now
+  @user.created_at = Time.now
+  @user.updated_at = Time.now
+  
+  if @user.save
+    Pony.mail(:to => @user.email, :via => :smtp, :via_options => {
+        :address => 'smtp.gmail.com',
+        :port => '587',
+        :enable_starttls_auto => true,
+        :user_name => 'epamrazor',
+        :password => 'Ep4mR4z0r',
+        :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
+        :domain => "HELO", # don't know exactly what should be here
+        },
+        :subject => 'Invite to ServiceBet', :body => erb(:invite, :layout => false)
+    ) 
+  end
+  redirect "/admin/users"
+end
+
+get "/admin/users" do
+  erb :adminusers
 end
--- controller.rb
+++ controller.rb
@@ -1,7 +1,7 @@
 require 'erb'
 require 'pony'
 
-before '/admin/?*' do
+before '/admin/*' do
   authorized = require_administrative_privileges
   session[:user] = 666 if authorized  #admin user_id is 666 ;)
   authorized
@@ -43,6 +43,10 @@ end
 get "/login" do
   session[:user] = nil
   erb :login, :layout => :layout_login
+end
+
+post "/login" do
+  "Not implemented yet"
 end
 
 get "/logout" do
