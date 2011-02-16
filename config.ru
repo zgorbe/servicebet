b9719cb45b64b59470ce4c8dfe36012d3b890ccd
zgorbe <gzoleemail@gmail.com>
2011-02-16 10:32:25 +0100

    DateTimePicker and asset_cache added

--- config.ru
+++ config.ru
@@ -1,3 +1,10 @@
 require 'servicebet'
+require 'utils/asset_cache'
+
+use AssetCache, {
+  /(\/css\/|\/images\/|\/js\/)/ =>
+  { :cache_control => "max-age=86400, public"}
+}
+
 
 run Sinatra::Application
---
+++ utils/asset_cache.rb
@@ -0,0 +1,23 @@
+class AssetCache
+  def initialize app, hash
+    @app = app
+    @hash = hash
+  end
+
+  def call env
+    res = @app.call(env)
+    path = env["REQUEST_PATH"]
+    @hash.each do |pattern, data|
+      if path =~ pattern
+        if data.has_key?(:cache_control)
+          res[1]["Cache-Control"] = data[:cache_control]
+        end
+        if data.has_key?(:expires)
+          res[1]["Expires"] = (Time.now + data[:expires]).utc.rfc2822
+        end
+        return res
+      end
+    end
+    res
+  end
+end
