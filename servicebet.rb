472a58a835a70bcf812bda1a9881239c295f6c34
zgorbe <gzoleemail@gmail.com>
2011-02-25 14:33:59 +0100

    New relic

--- servicebet.rb
+++ servicebet.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'newrelic_rpm'
 require 'dm-core'
 require 'do_mysql' if development?
 require 'do_postgres' if production?
--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ gem install do_postgres --version 0.10.1 --no-ri --no-rdoc
 gem install big_band --version 0.5.0 --no-ri --no-rdoc
 gem install pony --version 1.1 --no-ri --no-rdoc
 gem install hoptoad_notifier --version 2.4.6 --no-ri --no-rdoc
+gem install newrelic_rpm --no-ri --no-rdoc
